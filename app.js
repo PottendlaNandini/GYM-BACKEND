b2014e31fb75af76e2dda8368f4033b95ab1aaf5
Pottendla123 <pottendlanandini1@gmail.com>
2026-02-08 00:24:23 +0530

    first adding backend

---
+++ app.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import { sendEmail } from "./utils/sendEmail.js";
+
+config({ path: "./config.env" });  // Load env first
+
+const app = express();              // Create app FIRST
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+   cors({ 
+   origin: "http://localhost:5174",
+   methods: ["POST"],
+   credentials: true,
+   }) 
+);
+
+
+app.post("/send/mail", async (req, res) => {
+  console.log("BODY RECEIVED =>", req.body);
+
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all details",
+    });
+  }
+
+  try {
+    await sendEmail({
+      email: "pottendlanandini1@gmail.com",
+      subject: "GYM WEBSITE CONTACT",
+      message,
+      userEmail: email,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Message Sent Successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT}`);
+});
---
+++ utils/sendEmail.js
@@ -0,0 +1,27 @@
+import nodeMailer from "nodemailer";
+
+export const sendEmail = async (options) => {
+  console.log("📨 Trying to send email...");
+
+  const transporter = nodeMailer.createTransport({
+    service: process.env.SMTP_SERVICE, // gmail
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: options.email, // where YOU will receive email
+    subject: options.subject,
+    text: `${options.message}\n\nSender Email: ${options.userEmail}`,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("✅ Email sent:", info.response);
+  } catch (error) {
+    console.log("❌ Email error:", error.message);
+  }
+};
