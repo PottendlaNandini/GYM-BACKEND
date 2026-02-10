fde8734913c078fab7f418b3806256d1698e09ed
Pottendla123 <pottendlanandini1@gmail.com>
2026-02-10 22:25:36 +0530

    jj

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -12,8 +12,8 @@ export const sendEmail = async (options) => {
 
   const transporter = nodeMailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // VERY IMPORTANT (false for 587)
+  port: 465,
+  secure: true, // VERY IMPORTANT (false for 587)
   auth: {
     user: process.env.SMTP_MAIL,
     pass: process.env.SMTP_PASSWORD,
